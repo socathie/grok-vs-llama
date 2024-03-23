89414d44ded52c69ebe3d09c0aa0c18982aff41c
drCathieSo.eth <socathie@users.noreply.github.com>
2024-03-23 16:16:06 +0800

    Update hardhat.config.js and package.json with dotenv configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,25 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+const { API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: API_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
